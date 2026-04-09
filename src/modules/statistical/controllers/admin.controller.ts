b43d68757de100b40b5ec28d62964c0059ae8458
moaiadabdulsalam <moaiad.abdulsalam@gmail.com>
2026-04-09 20:12:57 +0300

    test

--- src/modules/statistical/controllers/admin.controller.ts
+++ src/modules/statistical/controllers/admin.controller.ts
@@ -5,26 +5,32 @@ import { RoleGuard } from 'src/core/guard/role.guard';
 import { Roles } from 'src/core/decorator/role.decorator';
 import { Role } from '@prisma/client';
 
-@UseGuards(JwtAuthGuard, RoleGuard)
-@Roles(Role.ADMIN)
+
+
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Get('/overview/total')
   total() {
     return this.adminService.total();
   }
-
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Get('/doctorsProfile')
   doctorsProfile() {
     return this.adminService.doctorsProfile();
   }
-
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Get('/doctor-speciality')
   speciality() {
     return this.adminService.doctorSpeciality();
   }
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
   @Get('/child-distribution')
   genderDistribution() {
     return this.adminService.genderDistribution();
